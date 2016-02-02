3dd3600bdecdfd75751277764c34eb98753a9182
PeterGuggisberg <pguggis567@aol.com>
2016-02-02 14:17:10 -0500

    Lab 3

--- DB Lab 3.sql
+++ DB Lab 3.sql
@@ -3,49 +3,49 @@
 --Database Management/Professor Labouseur
 --Lab 3
 
---1 fix
-SELECT *
+--1
+SELECT orders.ordnum, orders.totalusd
 FROM orders;
 
---2 fix
-SELECT *
+--2
+SELECT agents.name, agents.city
 FROM agents
 WHERE agents.name = 'Smith';
 
---3 fix
-SELECT *
+--3
+SELECT products.pid, products.name, products.priceusd
 FROM products
 WHERE products.quantity > 208000;
 
---4 fix
-SELECT *
+--4
+SELECT customers.name, customers.city
 FROM customers
 WHERE customers.city = 'Dallas';
 
---5 not done, revise!!!
-SELECT *
+--5
+SELECT agents.name
 FROM agents
-WHERE agents.city != 'New York'
-OR agents.city != 'Tokyo'
+WHERE agents.city <> 'New York'
+   AND agents.city <> 'Tokyo';
 
---6 not done, revise!!!
+--6
 SELECT *
 FROM products
-WHERE products.city != 'Dallas'
-OR products.city != 'Duluth'
-AND products.priceusd >= 1.00;
+WHERE products.city <> 'Dallas'
+   AND products.city <> 'Duluth'
+   AND products.priceusd >= 1.00;
 
 --7
 SELECT *
 FROM orders
 WHERE orders.mon = 'jan'
-OR orders.mon = 'mar';
+   OR orders.mon = 'mar';
 
 --8
 SELECT *
 FROM orders
 WHERE orders.mon = 'feb'
-AND orders.totalusd < 500.00;
+   AND orders.totalusd < 500.00;
 
 --9
 SELECT *
