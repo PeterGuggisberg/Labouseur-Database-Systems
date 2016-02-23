0e0fdd650db86435ad342c26ed84266aa3a38aee
PeterGuggisberg <pguggis567@aol.com>
2016-02-23 01:24:54 -0500

    Lab 5

--- DB Lab 5.sql
+++ DB Lab 5.sql
@@ -18,20 +18,37 @@ WHERE products.pid = orders.pid
 ORDER BY pid DESC;
 
 --3 Show the names of customers who have never placed an order. Use a subquery.
-SELECT products.pid
-FROM products, orders, customers
-WHERE products.pid = orders.pid
-    AND customers.cid = orders.cid
+SELECT distinct customers.name
+FROM customers
+WHERE not cid In (Select orders.cid
+	          From orders
+	         );
 
 --4 Show the names of customers who have never placed an order. Use an outer join.
-
+SELECT distinct customers.name
+FROM customers
+Left outer join orders
+on customers.cid = orders.cid
+Where orders.cid is null;
 
 --5 Show the names of customers who placed at least one order through an agent in their own city, along with those agents' names.
-
+SELECT customers.name, agents.name
+FROM customers, agents, orders
+WHERE customers.cid = orders.cid
+    AND agents.aid = orders.aid
+    AND customers.city = agents.city;
 
 --6 Show the names of customers and agents living in the same city, along with the name of the shared city, regardless of whether or not the customer has ever placed an order with that agent.
-SELECT 
-FROM 
-WHERE 
+SELECT distinct customers.name, customers.city, agents.name, agents.city
+FROM customers, agents, orders
+WHERE customers.city = agents.city;
 
---7 Show the name and city of customers who live in the city that makes the fewest different kinds of products. (Hint: Use count and group by on the Products table).
+--7 Show the name and city of customers who live in the city that makes the fewest different kinds of products.
+SELECT distinct customers.name, customers.city
+FROM customers, products
+WHERE customers.city In (select products.city 
+			 from products
+			 group by city
+			 order by count(quantity)
+			 limit 1
+			);
