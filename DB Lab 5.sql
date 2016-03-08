72c6cce0d1c3aafa3f4b9621b2db00c7cce104e5
PeterGuggisberg <pguggis567@aol.com>
2016-03-08 14:29:31 -0500

    Lab 5

--- DB Lab 5.sql
+++ DB Lab 5.sql
@@ -10,7 +10,7 @@ WHERE agents.aid = orders.aid
     AND orders.cid = 'c002';
 
 --2 Show the ids of products ordered through any agent who makes at least one order for a customer in Dallas, sorted by pid from highest to lowest. Use joins; no subqueries.
-SELECT Distinct o2.pid
+SELECT DISTINCT o2.pid
 FROM customers c, orders o1, orders o2
 WHERE c.cid = o1.cid
     AND o1.aid = o2.aid
@@ -18,18 +18,18 @@ WHERE c.cid = o1.cid
 ORDER BY o2.pid DESC;
 
 --3 Show the names of customers who have never placed an order. Use a subquery.
-SELECT distinct customers.name
+SELECT DISTINCT customers.name
 FROM customers
-WHERE cid nOT In (Select orders.cid
-	          From orders
+WHERE cid NOT IN (SELECT orders.cid
+	          FROM orders
 	         );
 
 --4 Show the names of customers who have never placed an order. Use an outer join.
-SELECT distinct customers.name
+SELECT DISTINCT customers.name
 FROM customers
-Left outer join orders
-on customers.cid = orders.cid
-Where orders.cid is null;
+LEFT OUTER JOIN orders
+ON customers.cid = orders.cid
+WHERE orders.cid is null;
 
 --5 Show the names of customers who placed at least one order through an agent in their own city, along with those agents' names.
 SELECT customers.name, agents.name
@@ -39,16 +39,16 @@ WHERE customers.cid = orders.cid
     AND customers.city = agents.city;
 
 --6 Show the names of customers and agents living in the same city, along with the name of the shared city, regardless of whether or not the customer has ever placed an order with that agent.
-SELECT distinct customers.name, customers.city, agents.name, agents.city
+SELECT DISTINCT customers.name, customers.city, agents.name, agents.city
 FROM customers, agents, orders
 WHERE customers.city = agents.city;
 
 --7 Show the name and city of customers who live in the city that makes the fewest different kinds of products.
-SELECT distinct customers.name, customers.city
+SELECT DISTINCT customers.name, customers.city
 FROM customers, products
-WHERE customers.city In (select products.city 
-			 from products
-			 group by products.city
-			 order by count(products.quantity) ASC
-			 limit 1
+WHERE customers.city IN (SELECT products.city 
+			 FROM products
+			 GROUP BY products.city
+			 ORDER BY count(products.quantity) ASC
+			 LIMIT 1
 			);
